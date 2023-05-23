8b784e4444ad3c099932e45024f32c67cb09375c
SeokyungCho <merrycho2003@gmail.com>
2023-05-23 15:53:32 +0900

    영어-한글-말한사람 모두 한번에 랜덤 출력할 수 있는 메소드 작성 - 오류 수정 필요(배열 불러올 수 없음)

--- app/src/main/java/com/example/dayleader/Quote.java
+++ app/src/main/java/com/example/dayleader/Quote.java
@@ -1,26 +1,46 @@
 package com.example.dayleader;
 
 public class Quote {
-    private String quote;
-    private String kor;
+    private String engs;
+    private String kors;
     private String author;
 
-
-    public Quote(String quote, String kor, String author) {
-        this.quote = quote;
-        this.kor = kor;
+    public Quote(String engs, String kors, String author) {
+        this.engs = engs;
+        this.kors = kors;
         this.author = author;
-    }
+    }//constructor 생성
 
-    public String getQuote() {
-        return quote;
-    }
+    public String getEngs() {
+        return engs;
+    }//getter 생성
+
+    public String getKors() {
+        return kors;
+    }//getter 생성
 
     public String getAuthor() {
         return author;
-    }
+    }//gether 생성
 
-    public String getKor() {
-        return kor;
-    }
+    public void setEngs(String engs) {
+        this.engs = engs;
+    }//setter 생성
+
+    public void setKors(String kors) {
+        this.kors = kors;
+    }//setter 생성
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }//setter 생성
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "engs='" + engs + '\'' +
+                ", kors='" + kors + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }// 출력할 명언의 모양 설정하기 - 마지막에 결과 출려해보고 수정할 것
 }
