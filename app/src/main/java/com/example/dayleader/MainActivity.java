83144d2d94b88917b83fde8e48f841787e34c635
jorippppong <duelee75@gmail.com>
2023-05-24 19:03:28 +0900

    Feat: Calendar 추가하고, 필요한 기능 추가

--- app/src/main/java/com/example/dayleader/MainActivity.java
+++ app/src/main/java/com/example/dayleader/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.dayleader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,20 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //캘린더 변수 설정
         MaterialCalendarView calendarView= findViewById(R.id.calendarView);
-        calendarView.getSelectedDate();
 
-        //캘린더
-        //calendarView.setOnDateChangedListener( {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-//                int formatYear = year;
-//                int formatMonth = month+1;
-//                int formatDay = day;
-//
-//                Log.d("main", Integer.toString(formatYear)+Integer.toString(formatMonth)+Integer.toString(formatDay));
-//            }
-        //});
+        //[캘린더] 현재 날짜로 설정
+        calendarView.setSelectedDate(CalendarDay.today());
+
+        //[캘린더] 선택한 날짜 가져 오기
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                // 선택된 날짜 정보 가져 오기
+                int year = date.getYear();
+                int month = date.getMonth();
+                int dayOfMonth = date.getDay();
+
+                // 선택된 날짜 정보 출력
+                String selectedDate = year + "-" + month + "-" + dayOfMonth;
+                Log.d("mainActivity", "calendar selectedDate: " + selectedDate);
+
+                //정보 가지고 다음 Activity 로 이동
+                Intent intent = new Intent(MainActivity.this, TodoActivity.class);
+                intent.putExtra("dateInfo", selectedDate);
+                startActivity(intent);
+            }
+        });
 
     }
 }
