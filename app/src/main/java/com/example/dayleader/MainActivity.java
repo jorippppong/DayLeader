0cae491db55ba83c655629f08ef4f57d4bfda27e
jorippppong <duelee75@gmail.com>
2023-05-24 18:17:50 +0900

    calendar 추가하고, 코드 수정

--- app/src/main/java/com/example/dayleader/MainActivity.java
+++ app/src/main/java/com/example/dayleader/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.dayleader;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialCalendarView calendarView= findViewById(R.id.calendarView);
+        calendarView.getSelectedDate();
+
+        //캘린더
+        //calendarView.setOnDateChangedListener( {
+//            @Override
+//            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+//                int formatYear = year;
+//                int formatMonth = month+1;
+//                int formatDay = day;
+//
+//                Log.d("main", Integer.toString(formatYear)+Integer.toString(formatMonth)+Integer.toString(formatDay));
+//            }
+        //});
+
     }
 }
