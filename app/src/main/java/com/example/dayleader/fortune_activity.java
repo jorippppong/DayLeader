741cb03946c3e0b59d52d1bd0e5169b94b866433
Hyung Suk Lee <corvallislee@naver.com>
2023-05-25 22:52:40 +0900

    포춘 쿠키 페이지 xml 및 수정 사항

---
+++ app/src/main/java/com/example/dayleader/fortune_activity.java
@@ -0,0 +1,14 @@
+package com.example.dayleader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fortune_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fortune);
+    }
+}
