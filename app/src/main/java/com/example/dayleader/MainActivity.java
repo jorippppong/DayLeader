8daff1133711f75ed60d716581f7995213981a39
Hyung Suk Lee <corvallislee@naver.com>
2023-05-31 17:29:52 +0900

    FIX: merge fix

--- app/src/main/java/com/example/dayleader/MainActivity.java
+++ app/src/main/java/com/example/dayleader/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.dayleader;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testButton = findViewById(R.id.testbutton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, fortune_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
